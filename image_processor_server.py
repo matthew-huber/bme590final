c49989effa01d8baa8d4dab185490efe6f1a27d5
Michael Postiglione <mjp59@duke.edu>
2018-11-28 21:56:04 -0500

    Fixed Base64 issue when sending json + started processing on server

--- image_processor_server.py
+++ image_processor_server.py
@@ -1,5 +1,6 @@
 from flask import Flask, jsonify, request
-
+from ImageProcessor import ImageProcessor
+import base64
 
 app = Flask(__name__)
 
@@ -13,8 +14,17 @@ def server_gui(processed_data):
 @app.route("/upload", methods=['POST'])
 def gui_server():
     r = request.get_json()
-
-    return
+    s1 = r.get("Images")
+    s2 = r.get("Process")
+    pro = ImageProcessor()
+    processed_images = []
+    if s2 == "Histogram Equalization":
+        for x in range(len(s1)):
+            processed_image = pro.histogramEqualization(
+                base64.b64decode(s1[x]))
+            processed_image_base64 = base64.b64encode(processed_image)
+            base64_string = processed_image_base64.decode('ascii')
+            processed_images.append(base64_string)
 
 
 if __name__ == "__main__":
