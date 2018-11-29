a857d7ac1bee8cccb5fd3b176b82d6d1d078dcc8
Michael Postiglione <mjp59@duke.edu>
2018-11-29 11:52:49 -0500

    Server can now receive info from gui, decoded it process it and encode it for send back

--- image_processor_server.py
+++ image_processor_server.py
@@ -1,6 +1,8 @@
 from flask import Flask, jsonify, request
 from ImageProcessor import ImageProcessor
 import base64
+import pickle
+import numpy as np
 
 app = Flask(__name__)
 
@@ -20,12 +22,12 @@ def gui_server():
     processed_images = []
     if s2 == "Histogram Equalization":
         for x in range(len(s1)):
-            processed_image = pro.histogramEqualization(
-                base64.b64decode(s1[x]))
+            nparr = np.frombuffer(base64.b64decode(s1[x]), np.uint8)
+            processed_image = pro.histogramEqualization(nparr)
             processed_image_base64 = base64.b64encode(processed_image)
             base64_string = processed_image_base64.decode('ascii')
             processed_images.append(base64_string)
-
+    return "woo"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0")
