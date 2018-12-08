4d1dfbd4826cfcf576a1a3e8ece60f8798cf1b66
Michael Postiglione <mjp59@duke.edu>
2018-12-08 13:09:34 -0500

    finished unit testing for server functions

--- test_image_processor_server.py
+++ test_image_processor_server.py
@@ -4,6 +4,9 @@ import numpy as np
 import datetime as dt
 import base64
 import matplotlib.image as mpimg
+import json
+from datetime import datetime
+from ImageProcessor import *
 
 
 @pytest.mark.parametrize("dict, is_valid", [
@@ -103,3 +106,85 @@ def test_addimagechars(img, type1, height, width, list1, list2):
     boo2 = (val2 == list2)
     boof = (boo1 and boo2)
     assert boof
+
+
+@pytest.mark.parametrize("img, timestamps, og_height, og_width, pro_images,"
+                         " pro_times, pro_height, pro_width,"
+                         " og_histo, pro_histo, boo",
+                         [
+                            ([10, 5], [10], [10], [200], [200, 50], [10], [10],
+                                [200], [10, 300], [30, 400], True),
+
+                            ([10, 5], [20], [], [200], [200, 50], [10], [],
+                             [200],
+                                [10, 300], [30, 400], True),
+
+                            ([], [20], [10], [200], [], [10], [10], [200],
+                                [10, 300], [30, 400], True),
+                         ])
+def test_server_gui(img, timestamps, og_height, og_width, pro_images,
+                    pro_times, pro_height, pro_width, og_histo,
+                    pro_histo, boo):
+    try:
+        j = server_gui(img, timestamps, og_height, og_width,
+                       pro_images, pro_times, pro_height,
+                       pro_width, og_histo, pro_histo)
+        json.loads(j)
+    except ValueError:
+        boof = False
+    boof = True
+    assert boof == boo
+
+
+@pytest.mark.parametrize("images, filenames, username, pro_times, pro_type,"
+                         " OG_height, OG_width, pro_height,"
+                         " pro_width, timestamps, image_list1",
+                         [
+                            ([1], ["Test1"], "Mike", [.10], ["Histo_equal"],
+                             [10], [10],
+                                [200], [10], [str(datetime.now())],
+                             ["Test1", "Test2", "Test3"]),
+
+                            ([1], ["Test2"], "Mike", [.10], ["Histo_equal"],
+                             [10], [10],
+                                [200], [10], [str(datetime.now())],
+                             ["Test1", "Test2", "Test3"]),
+
+                            ([1], ["Test3"], "Mike", [.10], ["Histo_equal"],
+                             [10], [10],
+                                [200], [10], [str(datetime.now())],
+                             ["Test1", "Test2", "Test3"]),
+                         ])
+def test_addimagestodatabase(images, filenames, username, pro_times, pro_type,
+                             OG_height, OG_width, pro_height, pro_width,
+                             timestamps, image_list1):
+    addImagesToDatabase(images, filenames, username, pro_times, pro_type,
+                        OG_height, OG_width, pro_height, pro_width, timestamps)
+    image_list = []
+    for image in DB_Image_Meta.objects.raw({"user": "Mike"}):
+        image_list.append(image.img_file_path)
+    assert image_list == image_list1
+
+
+pro = ImageProcessor()
+
+
+@pytest.mark.parametrize("img, proc_type, IP, output", [
+    (np.array([1, 2, 3]), "Histogram Equalization", pro,
+     np.array([0, 127, 255])),
+    (np.array([1, 2, 2, 5, 6]), "Contrast Stretching", pro,
+     np.array([0, 1844674407370955264, 1844674407370955264,
+               7378697629483821056, -9223372036854775808])),
+    (np.array([np.array([1, 2, 2, 5, 6, 8, 10]), np.array([1, 2,
+                                                           2, 5, 6,
+                                                           8, 10])]),
+     "Log Compression", pro,
+     np.array([np.array([0, 0, 0, 0, 0, 0, 0]), np.array([0, 0,
+                                                          0, 0, 0,
+                                                          0, 0])])),
+    (np.array([1, 2, 3]), "Reverse Video", pro, np.array([-2, -3, -4])),
+])
+def test_process(img, proc_type, IP, output):
+    print(process(img, proc_type, IP))
+    a = process(img, proc_type, IP) == output
+    assert a.all()
