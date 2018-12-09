432a1a71f3377cbf3bd1e04a95885c35bd1bacac
Katelyn <katelyn.offerdahl@duke.edu>
2018-12-09 11:05:54 -0500

    Adds basic unit tests for image_processor_gui whenever possible.

--- conftest.py
+++ conftest.py
@@ -1,8 +1,17 @@
 import pytest
 from ImageProcessor import ImageProcessor
+from image_processor_gui import App
+import numpy as np
+
 
 
 @pytest.fixture
 def IP():
     IP = ImageProcessor()
     return IP
+
+
+@pytest.fixture
+def GUI():
+    GUI = App()
+    return GUI
